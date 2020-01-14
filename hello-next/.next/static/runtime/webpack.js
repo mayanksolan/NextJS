77f66783c9a8172923521c7b114ce7e781fa547d
Mayank <mayanksolan@gitlab.com>
2020-01-14 12:18:53 +0530

    more changes

--- hello-next/.next/static/runtime/webpack.js
+++ hello-next/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d4a61966779707bd39ec";
+/******/ 	var hotCurrentHash = "0a906c6a1e9d0c3b0b2a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,7 +1,12 @@
-const Index = () => (
-    <div>
-        <p>Hello Next.js</p>
-    </div>
-)
+import Link from "next/link";
 
-export default Index
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a title="About Mayank Page">About Page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
